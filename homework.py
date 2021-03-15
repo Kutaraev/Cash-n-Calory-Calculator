c51d297a5c5954028020e676de9e867e09954ed4
Artem Kutaraev <artem.kutaraev@gmail.com>
2021-03-15 21:48:39 +0300

    Second version of program (with all corrections)

--- homework.py
+++ homework.py
@@ -1,4 +1,14 @@
 import datetime as dt
+DATE_FORMAT = '%d.%m.%Y'
+
+
+class Record:
+    def __init__(self, amount, comment, date=None):
+        if date is None:
+            date = dt.date.today().strftime(DATE_FORMAT)
+        self.date = (dt.datetime.strptime(date, DATE_FORMAT)).date()
+        self.amount = amount
+        self.comment = comment
 
 
 class Calculator:
@@ -10,73 +20,48 @@ class Calculator:
         self.records.append(record)
 
     def get_today_stats(self):
-        current_date = ((dt.datetime.now()).date())
+        current_date = dt.date.today()
         today_stats = 0
-        for i in self.records:
-            if i.date == current_date:
-                today_stats += i.amount
+        today_stats = sum(i.amount for i in self.records
+                          if i.date == current_date)
         return today_stats
 
     def get_week_stats(self):
         week_stats = 0
-        current_date = ((dt.datetime.now()).date())
-        time_delta = dt.timedelta(days=7)
+        current_date = dt.date.today()
+        time_delta = dt.timedelta(days=6)
+        week_ago = current_date - time_delta
         for i in self.records:
-            if (current_date - time_delta) <= i.date <= current_date:
+            if week_ago <= i.date <= current_date:
                 week_stats += i.amount
         return week_stats
+
+    def day_bal(self):
+        return self.limit - self.get_today_stats()
 
 
 class CashCalculator(Calculator):
     USD_RATE = 73.33
     EURO_RATE = 87.91
+    RUB_RATE = 1
 
     def get_today_cash_remained(self, currency):
-        cash_balance = self.limit - self.get_today_stats()
-        if currency == 'usd':
-            cash_final = round(cash_balance / self.USD_RATE, 2)
-            if cash_final > 0:
-                return f'На сегодня осталось {cash_final} USD'
-            elif cash_final == 0:
-                return 'Денег нет, держись'
-            else:
-                return f'Денег нет, держись: твой долг - {abs(cash_final)} USD'
-
-        elif currency == 'eur':
-            cash_final = \
-                round(cash_balance / self.EURO_RATE, 2)
-            if cash_final > 0:
-                return f'На сегодня осталось {cash_final} Euro'
-            elif cash_final == 0:
-                return 'Денег нет, держись'
-            else:
-                return \
-                    f'Денег нет, держись: твой долг - {abs(cash_final)} Euro'
-
-        elif currency == 'rub':
-            cash_final = (self.limit - self.get_today_stats())
-            if cash_final > 0:
-                return f'На сегодня осталось {cash_final} руб'
-            elif cash_final == 0:
-                return 'Денег нет, держись'
-            else:
-                return f'Денег нет, держись: твой долг - {abs(cash_final)} руб'
+        currency_rate = {'usd': (self.USD_RATE, 'USD'),
+                         'eur': (self.EURO_RATE, 'Euro'),
+                         'rub': (self.RUB_RATE, 'руб')}
+        cash_val = currency_rate.get(currency)
+        cash_final = round(self.day_bal() / cash_val[0], 2)
+        abs_final = abs(cash_final)
+        if cash_final > 0:
+            return f'На сегодня осталось {cash_final} {cash_val[1]}'
+        elif cash_final == 0:
+            return 'Денег нет, держись'
+        return f'Денег нет, держись: твой долг - {abs_final} {cash_val[1]}'
 
 
 class CaloriesCalculator(Calculator):
     def get_calories_remained(self):
-        delta_calories = self.limit - self.get_today_stats()
-        if delta_calories > 0:
+        if self.day_bal() > 0:
             return 'Сегодня можно съесть что-нибудь ещё, ' \
-                f'но с общей калорийностью не более {delta_calories} кКал'
-
-        else:
-            return 'Хватит есть!'
-
-
-class Record:
-    def __init__(self, amount, comment,
-                 date=((dt.datetime.now()).date()).strftime('%d.%m.%Y')):
-        self.amount = amount
-        self.comment = comment
-        self.date = (dt.datetime.strptime(date, '%d.%m.%Y')).date()
+                f'но с общей калорийностью не более {self.day_bal()} кКал'
+        return 'Хватит есть!'
